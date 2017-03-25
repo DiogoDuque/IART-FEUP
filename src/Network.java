3978caae7f1de500c48f3b0a419b257dd57e9d92
DiogoDuque <diogoduque13@hotmail.com>
2017-03-25 12:39:23 +0000

    changed network to multiLayerPerceptron
    for now, everything works with random inputs. Also, a new file is saved after running the network: it opens with NeurophStudio and allows a better visualization of the network

--- src/Network.java
+++ src/Network.java
@@ -1,7 +1,10 @@
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
-import org.neuroph.nnet.Perceptron;
+import org.neuroph.nnet.MultiLayerPerceptron;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Diogo on 22-03-2017.
@@ -9,36 +12,44 @@ import org.neuroph.nnet.Perceptron;
 public class Network {
     public static void main(String[] args){
         NeuralNetwork neuralNetwork = createNet();
+        neuralNetwork.save("bankruptcy_net.nnet"); //saves the last network version to file that can be opened with NeurophStudio
 
+        //EXAMPLE OF HOW TO QUICK-TEST THE NETWORK (RANDOM)
         //set network input
-        neuralNetwork.setInput(0, 1);
+        double[] inputs = new double[64];
+        Random r = new Random();
+        for(int i=0; i<64; i++)
+            inputs[i]= r.nextDouble();
+        neuralNetwork.setInput(inputs);
         //calculate network
         neuralNetwork.calculate();
         //get network output
         double[] networkOutput = neuralNetwork.getOutput();
-        System.out.println(networkOutput[0]);
+        System.out.println("NeuralNet Result: "+networkOutput[0]);
 
     }
 
-    public static NeuralNetwork createNet(){
-        //create new perceptron network
-        NeuralNetwork neuralNetwork = new Perceptron(64, 1);
+    public static NeuralNetwork createNet() {
+        //create new network
+        ArrayList<Integer> layers = new ArrayList<>();
+        layers.add(64);
+        layers.add(10);
+        layers.add(1);
+        NeuralNetwork neuralNetwork = new MultiLayerPerceptron(layers);
 
         //create training set
-        DataSet trainingSet =
-                new DataSet(64, 1);
-        //add training data to training set (inputs,outputs)
-        /*trainingSet. addRow (new DataSetRow(new double[]{0, 0},
-                new double[]{0}));
-        trainingSet. addRow (new DataSetRow (new double[]{0, 1},
-                new double[]{1}));
-        trainingSet. addRow (new DataSetRow (new double[]{1, 0},
-                new double[]{1}));
-        trainingSet. addRow (new DataSetRow (new double[]{1, 1},
-                new double[]{1}));*/
+        DataSet trainingSet = new DataSet(64, 1);
+        Random r = new Random();
+        //add training data to training set RANDOM
+        for(int i=0; i<10; i++){
+            double[] inputs = new double[64];
+            for(int j=0; j<64; j++)
+                inputs[j]=r.nextDouble();
+            trainingSet.addRow(new DataSetRow(inputs, new double[]{r.nextInt(2)}));
+        }
 
         //learn the training set
-        neuralNetwork.learn(trainingSet);
+        //neuralNetwork.learn(trainingSet);
 
         return neuralNetwork;
     }
